09bba4e863630dcb09599738ff0260860e663e70
Niels Van Haren <niels@response.nl>
2020-01-12 14:54:39 +0100

    git ignore fixed

---
+++ android/build/generated/source/buildConfig/debug/nl/response/capacitor/cameraoverlay/capacitorcameraoverlay/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package nl.response.capacitor.cameraoverlay.capacitorcameraoverlay;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "nl.response.capacitor.cameraoverlay.capacitorcameraoverlay";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
